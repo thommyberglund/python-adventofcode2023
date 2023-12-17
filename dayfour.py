7dc1ade1ea336e5663fd10c15c537eb8b9edcdd9
Thommy <thommy.berglund@gmail.com>
2023-12-17 21:30:18 +0100

    Initial commit

--- dayfour.py
+++ dayfour.py
@@ -1,18 +1,6 @@
 file = open('input.txt', 'r')
 lines = file.read().splitlines()
-
-card_counter = [1] * len(lines)
-total_lines = 0
-
-for index, line in enumerate(lines):
-   matches = []
-   cardnumber = line.lstrip("Card")[:4].strip()
-   numbers = line.lstrip("Card")[5:]
-   winningnumbers = numbers.partition("|")[0].split()
-   mynumbers = numbers.partition("|")[2].split()
-   for number in winningnumbers:
-      if number in mynumbers:
-         matches.append(number)
-   for n in range(len(matches)):
-      card_counter[index + n  + 1] += card_counter[index]
-print(str(sum(card_counter)))
+print(lines[0])
+print("Line 1: " + lines[1])
+print("Line 2: " + lines[2])
+print(lines[0].split())
